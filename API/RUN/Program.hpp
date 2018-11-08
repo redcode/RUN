3e1e91348a16003858adb23a36db18768f5cfebc
redcode <manuel@zxe.io>
2018-11-08 08:25:02 +0100

    LGPLv3 headers.

--- API/RUN/Program.hpp
+++ API/RUN/Program.hpp
@@ -1,9 +1,21 @@
-/* RUN Kit C++ API - Program.hpp
+/* RUN Kit API - Program.hpp
   _____  __ ______  ___
  /   - )/  /  /   \/  /
 /__/\__/_____/__/\___/ Kit
 Copyright (C) 2016-2018 Manuel Sainz de Baranda y Goñi.
-Released under the terms of the GNU Lesser General Public License v3. */
+-------------------------------------------------------------------------------
+This library is  free software: you can redistribute it  and/or modify it under
+the terms  of the GNU  Lesser General Public License  as published by  the Free
+Software Foundation, either  version 3 of the License, or  (at your option) any
+later version.
+
+This library is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+You should have received  a copy of the GNU Lesser General Public License along
+with this library. If not, see <http://www.gnu.org/licenses/>.
+---------------------------------------------------------------------------- */
 
 #ifndef _RUN_Program_HPP_
 #define _RUN_Program_HPP_
